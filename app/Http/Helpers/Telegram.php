99071488d0c9fef1442d38d6bb90083a497cbef8
MMR <moj2raj2@gmail.com>
2024-04-22 21:55:31 +0330

    edit orders

--- app/Http/Helpers/Telegram.php
+++ app/Http/Helpers/Telegram.php
@@ -107,10 +107,10 @@ class Telegram
     static function creator($method, $datas = [])
     {
         if (!str_contains(url('/'), '.com') && !str_contains(url('/'), '.ir')) return;
-//        $url = "https://api.telegram.org/bot" . env('TELEGRAM_BOT_TOKEN', '') . "/" . $method;
+        $url = "https://api.telegram.org/bot" . env('TELEGRAM_BOT_TOKEN', '') . "/" . $method;
 
-        $url = "https://2sport.ir/api/dabelchin_telegram";
-        $datas['cmnd'] = $method;
+//        $url = "https://2sport.ir/api/dabelchin_telegram";
+//        $datas['cmnd'] = $method;
 
 
 //        $ch = curl_init();
